bfe39e3c78661209fd4c4856676ccf041a89e699
amer1can <amer1can@mail.ru>
2021-10-26 15:27:29 +0300

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <!-- navigation -->
+    <nav class="navbar has-shadow" role="navigation" aria-label="main navigation">
+      <div class="container">
+        <div class="navbar-start">
+          <nuxt-link to="/" class="navbar-item">
+            Vue-Nuxt Site
+          </nuxt-link>
+          <nuxt-link active-class="is-active" to="/" class="navbar-item is-tab" exact>Home</nuxt-link>
+          <nuxt-link active-class="is-active" to="/about" class="navbar-item is-tab" exact>About</nuxt-link>
+        </div>
+      </div>
+    </nav>
+    <!-- /navigation -->
+
+    <!-- displays the page component -->
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DefaultPage"
+}
+</script>
+
+<style scoped>
+.main-content {
+  margin: 30px 0;
+}
+</style>
---
+++ pages/features.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "features"
+}
+</script>
+
+<style scoped>
+
+</style>
